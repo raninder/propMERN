7f51a8323d07c32f9e55edb6364ea47a781fcd3f
Raninder Dhillon <raninderdhillon@gmail.com>
2024-01-15 11:44:39 -0500

    highlights updated

--- buyer/src/components/Highlights.js
+++ buyer/src/components/Highlights.js
@@ -6,29 +6,55 @@ const Highlights = () => {
 	return (
 		<div className="highlights-container">
 			<h3> Home Highlights</h3>
-			<table className="highlights-table">
-				<tr>
-					<td className="align-left"><FontAwesomeIcon icon={faSquareParking} /><span className="align">Parking</span></td>
-					<td className="align-right">4 Parking Spaces</td>
+			<div className="highlights-content">
+				<div className="column1">
+					<div className="key">
+						<FontAwesomeIcon icon={faSquareParking} /><span className="key-icon">Parking</span>
+					</div>
+					<div className="key">
+						<FontAwesomeIcon icon={faCircle} /><span className="key-icon">Basement</span>
+					</div>
+					<div className="key">
+						<FontAwesomeIcon icon={faCircle} /><span className="key-icon">Cooling</span>
+					</div>
+				</div>
 
-					<td className="align-left"><FontAwesomeIcon icon={faCircle} /><span className="align">HOA</span> </td>
-					<td className="align-right">None </td>
-				</tr>
-				<tr>
-					<td className="align-left" ><FontAwesomeIcon icon={faCircle} /><span className="align">Basement</span></td>
-					<td className="align-right">Full Finished</td>
-					<td className="align-left"><FontAwesomeIcon icon={faCircle} /><span className="align">Price/Sqft</span> </td>
-					<td className="align-right">235$ </td>
-				</tr>
-			
-				<tr>
-					<td className="align-left"><FontAwesomeIcon icon={faCircle} /><span className="align">Cooling</span></td>
-					<td className="align-right">Yes</td>
-					<td className="align-left"><FontAwesomeIcon icon={faCircle} /><span className="align">Listed</span> </td>
-					<td className="align-right">70 days ago </td>
-				</tr>
-			
-			</table>
+				<div className="column2">
+					<div className="key">
+						<span className="key-icon">4 Parking Spaces</span>
+					</div>
+					<div className="key">
+					<span className="key-icon">full,finished</span>
+					</div>
+					<div className="key">
+						<span className="key-icon">Yes</span>
+					</div>
+				</div>
+
+				<div className="column3">
+					<div className="key">
+						<FontAwesomeIcon icon={faCircle} /><span className="key-icon">HOA</span>
+					</div>
+					<div className="key">
+						<FontAwesomeIcon icon={faCircle} /><span className="key-icon">Price/Sqft</span>
+					</div>
+					<div className="key">
+						<FontAwesomeIcon icon={faCircle} /><span className="key-icon">Listed</span>
+					</div>
+				</div>
+
+				<div className="column4">
+					<div className="key">
+						<span className="key-icon">None</span>
+					</div>
+					<div className="key">
+					<span className="key-icon">235$</span>
+					</div>
+					<div className="key">
+						<span className="key-icon">70 days ago</span>
+					</div>
+				</div>
+			</div>	
 		</div>
 	);
 }
