19cbf1067f7990aeb2f7b8fd2fa6754e99e665c8
Raninder Dhillon <raninderdhillon@gmail.com>
2024-01-16 12:32:32 -0500

    Update Navbar.js

--- buyer/src/components/Navbar.js
+++ buyer/src/components/Navbar.js
@@ -1,15 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 import '../styles/nav.css'
+
 const Navbar = () => {
 	return ( 
 		<>
       <header>
         <div className='nav-container nav-flex'>
-          <div className='logo'>
+          <div className='navlogo'>
             <img src='https://hosting.renderforestsites.com/21754072/1065231/media/943bd97358ebabb806a8aa1f5a171982.png' alt='' />
           </div>
-					<form>
-              <input class="search" type="search" placeholder="Search.."/> 
-      
+					<form className="search-form">
+            <input  type="search" className="search" placeholder="Search.."/> 
+						<FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass} />
 						</form>
 				</div>
 			</header>
