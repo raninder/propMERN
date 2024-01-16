57a88a7eed35818c64953888ea46902ea05ded45
Raninder Dhillon <raninderdhillon@gmail.com>
2024-01-16 12:33:58 -0500

    Update Price.js

--- buyer/src/components/Price.js
+++ buyer/src/components/Price.js
@@ -18,7 +18,7 @@ const Price = ({data}) => {
 					</p>
 			</div>
 			<div className="map">
-					<iframe title="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5771.918406738923!2d-79.37839350590437!3d43.6698182844049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb51a3891935%3A0xa11c73fe81eacdd0!2sSt.%20James%20Town%2C%20Toronto%2C%20ON!5e0!3m2!1sen!2sca!4v1703854898028!5m2!1sen!2sca" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+					<iframe title="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2885.983726430692!2d-79.3751554889926!3d43.66930827098086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb51a9d21909%3A0x6992d9ceb65fdcb4!2sSt%20James%20Ave%2C%20Toronto%2C%20ON!5e0!3m2!1sen!2sca!4v1705423951476!5m2!1sen!2sca" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 			</div>
 		</div>
 	);
